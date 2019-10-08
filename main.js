071864bae8de5665108dc2ee090abc0d029a7b11
Ryan Tablada <ryan.tablada@gmail.com>
2019-10-08 13:24:22 -0500

    wip: make git-janitor interactive

--- main.js
+++ main.js
@@ -1,9 +1,37 @@
 import cleanMergedBranches from './lib/clean-merged-branches';
 import yargs from 'yargs';
+import inquirer from 'inquirer';
+import getRemoteNames from './lib/git-commands/get-remote-names';
 
-const argv = yargs.argv;
+async function main() {
+  const argv = yargs.argv;
+  let remoteNames = await getRemoteNames();
 
-const defaultBranchName = argv.defaultBranch || process.DEFAULT_BRANCH_NAME || 'master';
-const dryRun = argv.dryRun;
+  let { compareSources, defaultBranchName, dryRun } = await inquirer.prompt([
+    {
+      type: 'checkbox',
+      message: 'What sources do you want to clean?',
+      name: 'compareSources',
+      choices: [
+        { name: 'Local branches', value: { local: true }, checked: true },
+        ...remoteNames.map((r) => ({ name: `Remote: ${r}`, value: { remote: true, remoteName: r } })),
+      ],
+    },
+    {
+      type: 'input',
+      message: 'What branch are features merged into?',
+      name: 'defaultBranchName',
+      default: argv.defaultBranch || process.DEFAULT_BRANCH_NAME || 'master',
+    },
+    {
+      type: 'confirm',
+      message: 'Do you want to run a dry run (only log changes)?',
+      name: 'dryRun',
+      default: true,
+    },
+  ]);
 
-cleanMergedBranches({ defaultBranchName, dryRun });
+  cleanMergedBranches({ compareSources, defaultBranchName, dryRun });
+}
+
+main();
